dc8f54103e81d9198a29c056ba4542bbe7a18af7
Ahmed Ali <ahmaad.alii.213@gmail.com>
2026-03-24 11:13:50 +0300

    Create Final Account Balance.sql
    PayPal - Final Account Balance Solution

---
+++ DataLemur/Easy/Final Account Balance.sql
@@ -0,0 +1,9 @@
+SELECT account_id,
+        SUM(
+            CASE
+                WHEN transaction_type = 'Deposit' THEN amount
+                WHEN transaction_type = 'Withdrawal' THEN -amount   -- to subtract when it is 'Withdrawal'
+            END 
+            ) AS final_balance
+FROM transactions
+GROUP BY account_id
