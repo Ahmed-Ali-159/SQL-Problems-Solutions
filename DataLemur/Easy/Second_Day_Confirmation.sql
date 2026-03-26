9097dfcddca8fd75f5a21667427ce0c9958b10f2
Ahmed Ali <ahmaad.alii.213@gmail.com>
2026-03-26 09:53:29 +0300

    26/3/2026

---
+++ DataLemur/Easy/Second_Day_Confirmation.sql
@@ -0,0 +1,6 @@
+-- Using Postgres
+
+SELECT e.user_id
+FROM emails e INNER JOIN texts t 
+ON e.email_id = t.email_id
+WHERE signup_action = 'Confirmed' AND t.action_date = e.signup_date + INTERVAL '1 day'
